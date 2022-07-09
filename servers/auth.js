b4e2624d6e94d69c5ec810cc618fd63abbb327f3
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-08 21:10:08 -0700

    installed mongoose

--- servers/auth.js
+++ servers/auth.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 
 const express = require("express")
-const { MongoClient } = require("mongodb")
+
 const UserModel = require("./model/user")
 
+
+//MongoDB config
 const URL_DB = process.env.URL_DB
 
 
+
 const server = express()
+
+const r = await 
 
 server.listen(5000, () => {
     console.log('Auth server running on port 5000')
