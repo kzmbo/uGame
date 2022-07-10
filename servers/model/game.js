92294279d9ef70686bfe3e5323939b290676f178
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-10 04:13:38 -0700

    created game schema

---
+++ servers/model/game.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const GameSchema = new mongoose.Schema({
+    game_title: {
+        type: String
+    },
+    game_status: {
+        type: String
+    },
+    game_rating: {
+        type: Number
+    },
+    game_screenshot_uri: {
+        type: String
+    },
+    game_release_date: {
+        type: String
+    }
+})
+
+const GameModel = mongoose.model('game', GameSchema)
+module.exports = GameModel
