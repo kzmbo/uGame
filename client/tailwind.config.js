817ed60bd48a6fa484af484c5261cf8a2d5d0c70
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-14 13:03:02 -0700

    added src and color pallete for tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#151515',
+        'blue': '#6AC9FB',
+        'white': '#E4E4E4'
+      }
+    },
   },
   plugins: [],
 }
