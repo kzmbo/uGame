8867fd6a2bb8f87d0bf5fc7f447ec9b77237bf56
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-15 21:09:48 -0700

    included UserPerstance component

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,34 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import Login from './views/Login';
 import Signup from './views/Signup';
 import Dashboard from './views/Dashboard'
-import {AuthContext} from './context/AuthProvider';
+import UserPersistance from './components/UserPersistance';
+import { AuthContext } from './context/AuthProvider';
 import { useState, useContext } from 'react';
 
 
 function App() {
-  const [isAuth, setAuth] = useState()
+  const [authUser, setAuth] = useState({})
+  Axios.defaults.withCredentials = true
 
   return (
-    <AuthContext.Provider value={{isAuth, setAuth}}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthContext.Provider value={{authUser, setAuth}}>
         <Routes>
+          
           <Route path='/' index element={<Login />} />
           <Route path='/login' element={<Login />}/>
           <Route path='/signup' element={<Signup />} />
+
+          <Route element={<UserPersistance />}>
+            <Route element={<ProtectedRoutes />}>
+              <Route path='/dashboard' element={<Dashboard />}/>
+            </Route>
+          </Route>
+
         </Routes>
-      </BrowserRouter>
-    </AuthContext.Provider>
+      </AuthContext.Provider>
+    </BrowserRouter>
+ 
  
   );
 }
