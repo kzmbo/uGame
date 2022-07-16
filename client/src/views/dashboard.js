e694792aa5f24e35b89ed47d99b4a4825fe2e150
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-16 13:45:31 -0700

    added useParams

--- client/src/views/dashboard.js
+++ client/src/views/dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Dashboard = () => {
+  const { userId } = useParams()
+  
   return (
     <div>
         this is the dashboard
