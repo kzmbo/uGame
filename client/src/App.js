f1c49207203d9d4ec3bfb4960b549fc75ecdfb51
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-15 17:18:19 -0700

    added auth context

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import Axios from 'axios'
 import { BrowserRouter, Routes, Route, Link,  } from "react-router-dom"
+import ProtectedRoutes from './components/ProtectedRoutes';
 import Login from './views/Login';
 import Signup from './views/Signup';
+import Dashboard from './views/Dashboard'
+import {AuthContext} from './context/AuthProvider';
+import { useState, useContext } from 'react';
+
 
 function App() {
+  const [isAuth, setAuth] = useState()
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' index element={<Login />} />
-        <Route path='/login' element={<Login />}/>
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{isAuth, setAuth}}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index element={<Login />} />
+          <Route path='/login' element={<Login />}/>
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+ 
   );
 }
 
