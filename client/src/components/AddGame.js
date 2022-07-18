6a479992c4fa1d59d14a0b37c12f0e7b3e4ba112
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-18 15:34:16 -0700

    styled add game page

--- client/src/components/AddGame.js
+++ client/src/components/AddGame.js
@@ -1,19 +1,44 @@
 import React, { useContext } from 'react'
 import { DisplayAddGame } from '../context/DisplayAddGame'
+import './../style/AddGamePage.css';
 
 const AddGame = () => {
     const { displayAddGame, setDisplayAddGame} = useContext(DisplayAddGame)
 
-  return (
-    <div className='fixed inset-0 bg-white opacity-90 w-full h-screen'>
-        <button onClick={(e) => {
-            e.preventDefault()
-            setDisplayAddGame(false)
-        }}>
-            Exit
-        </button>
-    </div>
-  )
+    const addToPlayed = () => {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+    const addToWishlish = () => {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+
+
+    return (
+        <div className='fixed inset-0 bg-white opacity-90 w-full h-screen'>
+                <button className='exit-btn' onClick={(e) => {
+                    e.preventDefault()
+                        setDisplayAddGame(false)
+                    }}>
+                        Exit
+                </button>
+            <div className='flex items-center justify-center h-screen'>
+                <button className='select-btn'>Add to Played</button>
+                <p className='border border-black h-24'></p>
+                <button className='select-btn'>Add to Wishlist</button>
+            </div>
+          
+        </div>
+    )
 }
 
 export default AddGame
