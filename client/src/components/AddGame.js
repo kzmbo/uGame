4da72c103879c7d077b020ce36d44de02e8bebf9
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-18 20:46:32 -0700

    able to add games

--- client/src/components/AddGame.js
+++ client/src/components/AddGame.js
@@ -4,47 +4,89 @@ import React, { useContext, useState } from 'react'
 import { DisplayAddGame } from '../context/DisplayAddGame'
 import Axios from 'axios' 
 import './../style/AddGamePage.css';
-import Autocomplete from 'react-autocomplete'
+import { AuthContext } from '../context/AuthProvider'
+import { useNavigate } from 'react-router-dom'
 
 const AddGame = () => {
-    const { displayAddGame, setDisplayAddGame} = useContext(DisplayAddGame)
-    const [gameTitle, setGameTitle] = useState('')
+    const navigate = useNavigate()
 
-    const game_response = async () => {
-        if (gameTitle.length === 0) return
-        await Axios.get(`https://api.rawg.io/api/games/${gameTitle}?key=c65af6735319413f81c8009fee466c76`)
+    const { authUser } = useContext(AuthContext)
+
+    const { displayAddGame, setDisplayAddGame} = useContext(DisplayAddGame)
+
+    const [endpointMsg, setEndpointStatus] = useState('')
+
+    const addGameURI = 'http://localhost:4000/addplayedgame'
+
+    const addGameToDB = async (gameTitle, gameStatus, gameRating, gameHoursPlayed) => {
+        console.log('running')
+        if (gameTitle.length === 0 || gameStatus === null || gameRating === null || gameHoursPlayed === null) return setEndpointStatus('Unable to add game. Please fill in all The fields')
+        await Axios.get(`https://api.rawg.io/api/games/${gameTitle}?key=c65af6735319413f81c8009fee466c76`, {
+            withCredentials: false
+        })
+        .then(async (response) => {
+            if (response.status !== 200) return setEndpointStatus('Unable to add game. Please fill in all The fields')
+            if (response.status === 404) return setEndpointStatus('Unable to find game. Please Try Again.') 
+            let game = response.data
+
+            await Axios.post(addGameURI, {
+                userID: authUser.userID,
+                game: {
+                    game_title: game.name,
+                    game_status: gameStatus,
+                    game_rating: gameRating,
+                    game_screenshot_uri: game.background_image,
+                    game_release_date: game.released,
+                    game_hours_played: gameHoursPlayed
+                }
+            })
+            .then((response) => {
+                console.log(response)
+                setEndpointStatus('Successfully Added Game to library.') 
+                window.location.reload()
+            })
+            .catch((error) => {
+                console.log(error)
+                return setEndpointStatus('Unable to find game. Please Try Again.') 
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
-    
+
 
     const AddToPlayed = () => {
+        const [gameTitle, setGameTitle] = useState('')
+        const [gameStatus, setGameStatus] = useState(null)
+        const [gameRating, setGameRating] = useState(0)
+        const [gameHoursPlayed, setGameHoursPlayed] = useState(0)
         return(
             <div className='grid grid-cols-1 place-content-center'>
-                <label className='text-field-label-game'>Game Title</label>
-                {/* <input type="text" className='text-field-add-game w-3/4' /> */}
-                <Autocomplete 
-                    getItemValue={(item) => item.name}
-                    items={game_response}
-                    renderItem={(item, isHighlighted) =>
-                        <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-                            {item.label}
-                        </div>
-                    }
-                    value={value}
-                    onChange={(e) => value = e.target.value}
-                    onSelect={(val) => value = val}
-                />
-                <label className='text-field-label-game'>Rating (X / 10)</label>
-                <input type="text" className='text-field-add-game w-1/5' />
-                <label className='text-field-label-game'>Hours Played</label>
-                <input type="text" className='text-field-add-game w-1/5' />
-                <label className='text-field-label-game'>Status: </label>
-                <select class="status-dropmenu" name="status">
-                    <option value="Currently Playing">Currently Playing</option>
-                    <option value="Completed">Completed</option>
-                    <option value="In Process">In Process</option>
-                    <option value="Dropped">Dropped</option>
-                </select>
-                <button className='add-game-btn'>Add Game!</button>
+                <form>
+                    <label className='text-field-label-game'>Game Title</label>
+                    <input type="text" className='text-field-add-game w-3/4' onChange={e => setGameTitle(e.target.value)} />
+
+                    <label className='text-field-label-game'>Rating (X / 10)</label>
+                    <input type="text" className='text-field-add-game w-1/5' onChange={e => setGameRating(e.target.value)} />
+
+                    <label className='text-field-label-game'>Hours Played</label>
+                    <input type="text" className='text-field-add-game w-1/5' onChange={e => setGameHoursPlayed(e.target.value)}/>
+
+                    <label className='text-field-label-game'>Status: </label>
+                    <select className="status-dropmenu" onChange={e => setGameStatus(e.target.value)} name="status">
+                        <option value="Currently Playing">Currently Playing</option>
+                        <option value="Completed">Completed</option>
+                        <option value="In Process">In Process</option>
+                        <option value="Dropped">Dropped</option>
+                    </select>
+                    <button className='add-game-btn' onClick={(e) => {
+                            e.preventDefault()
+                            addGameToDB(gameTitle, gameStatus, gameRating, gameHoursPlayed)
+                        }}>Add Game!</button>
+                </form>
+                
+                <h1 className='mx-auto text-xl'>{endpointMsg}</h1>
             </div>
         )
     }
