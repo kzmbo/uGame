cbaf82fd7a2880749bfb4666ffdee3cf44f86b69
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-19 23:10:58 -0700

    added metascore and platform for wishlist games

--- client/src/components/GameWishlist.js
+++ client/src/components/GameWishlist.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+const RenderPlatforms = ({platforms}) => {
+  return platforms.map((plat, i) => {
+    return (
+      <p className='text-lg px-1 font-normal' key={i}>{plat}</p>
+    )
+  })
+}
+
 const GameWishlist = ({ GamesWishlist }) => {
     return GamesWishlist.map((game, index) => {
         return(
@@ -12,12 +20,17 @@ const GameWishlist = ({ GamesWishlist }) => {
               <div className='dashboard-game-textbox relative'>
                 <div className='dashboard-game-textbox-title-rating-section'>
                   <h1 className='dashboard-game-title'>{game.game_title}</h1>
+                  <h1 className='dashboard-game-rating w-full text-sm'>Metacritic: {game.game_metacritic} / 100</h1>
                 </div>
                 <div className='dashboard-game-textbox-subinfo-section'>
                     <h1 className='dashboard-game-subtext-heading'>Released Date: <p className='dashboard-game-subtext-info'>{game.game_release_date}</p></h1>
                     <div>
-                        <h1 className='dashboard-game-subtext-heading'>Available On: </h1>
-                        {}
+                        <h1 className='dashboard-game-subtext-heading'>Available On: 
+                          <div className='flex justify-start px-1'>
+                            {<RenderPlatforms platforms={game.game_platforms} />}
+                          </div>
+                        </h1>
+                        
                     </div>
                 </div>
 
