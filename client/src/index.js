38de8553ef14a1360f6508eb35ac6b1d3b6ac322
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-20 16:41:55 -0700

    import all css files

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './style/AddGamePage.css'
+import './style/Dashboard.css'
+import './style/GameCard.css'
+import './style/Login_Signup.css'
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
