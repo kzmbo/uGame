d0f247b70ae1defd29c8e2aa885f69ce91ffdcb4
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-20 15:09:12 -0700

    added state and new section for edit game pg

--- client/src/views/dashboard.js
+++ client/src/views/dashboard.js
@@ -4,18 +4,24 @@ import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../context/AuthProvider'
 import { useParams, useNavigate } from 'react-router-dom'
 import { DisplayAddGame } from '../context/DisplayAddGame'
+import { DisplayEditGame } from '../context/DisplayEditGame'
 import AddGame from '../components/AddGame'
 import GamePlayed from '../components/GamePlayed'
 import GameWishlist from '../components/GameWishlist'
+import EditGame from '../components/EditGame'
 
 const Dashboard = () => {
   const navigate = useNavigate()
 
   const { userId } = useParams()
   const { authUser, setAuth } = useContext(AuthContext)
-  const [ isDisplayingPlayedGames, setGameDisplayStatus ] = useState(false)
+  const [ isDisplayingPlayedGames, setGameDisplayStatus ] = useState(true)
   const [ displayAddGame, setDisplayAddGame ] = useState(false)
-  const [ displayEditGame, setDisplayEditGame ] = useState(false)
+  const [ displayEditGame, setDisplayEditGame ] = useState({
+    display: false,
+    gameID: '',
+    gameTitle: ''
+  })
 
   const uid = authUser?.userID
   const sid = authUser?.sid
@@ -35,61 +41,63 @@ const Dashboard = () => {
   }
 
   return (
-    <DisplayAddGame.Provider value={{displayAddGame, setDisplayAddGame}}>
-      <div className='bg-dashboard relative'>
-        <div className='bg-white pb-5 w-full overflow-hidden'>
-          <div className='dashboard-header'>
-            <h1 className='dashboard-header-logo'>uGame</h1>
-            <div className='dashboard-header-logout-section'>
-              <p className='dashboard-header-signin-status '>Hello, {username} </p>
-              <button className='dashboard-logout-btn' onClick={logoutUser}>Logout</button>
+    <DisplayEditGame.Provider value={{displayEditGame, setDisplayEditGame}}>
+      <DisplayAddGame.Provider value={{displayAddGame, setDisplayAddGame}}>
+        <div className='bg-dashboard relative'>
+          <div className='bg-white pb-5 w-full overflow-hidden'>
+            <div className='dashboard-header'>
+              <h1 className='dashboard-header-logo'>uGame</h1>
+              <div className='dashboard-header-logout-section'>
+                <p className='dashboard-header-signin-status '>Hello, {username} </p>
+                <button className='dashboard-logout-btn' onClick={logoutUser}>Logout</button>
+              </div>
+          </div>
+
+            <div className='my-2'></div>
+
+            <div className='dashboard-select-section'>
+              <button className={isDisplayingPlayedGames ? "btn_ACTIVE" : "dashboard-select-btn"} onClick={(e) => {
+                e.preventDefault()
+                setGameDisplayStatus(true)
+              }}>Library</button>
+              <div className='dashboard-line'></div>
+              <button className={!isDisplayingPlayedGames ? "btn_ACTIVE" : "dashboard-select-btn"} onClick={(e) => {
+                e.preventDefault()
+                setGameDisplayStatus(false)
+              }}>Wishlist</button>
             </div>
-        </div>
 
-          <div className='my-2'></div>
+            <div className='my-2'></div>
 
-          <div className='dashboard-select-section'>
-            <button className={isDisplayingPlayedGames ? "btn_ACTIVE" : "dashboard-select-btn"} onClick={(e) => {
-              e.preventDefault()
-              setGameDisplayStatus(true)
-            }}>Library</button>
-            <div className='dashboard-line'></div>
-            <button className={!isDisplayingPlayedGames ? "btn_ACTIVE" : "dashboard-select-btn"} onClick={(e) => {
-              e.preventDefault()
-              setGameDisplayStatus(false)
-            }}>Wishlist</button>
+            <div className='text-center'>
+              <label className='dashboard-text-field-label'>Search Titles</label>
+              <input type='text' className='dashboard-text-field '/>
+            </div>
           </div>
 
           <div className='my-2'></div>
-
-          <div className='text-center'>
-            <label className='dashboard-text-field-label'>Search Titles</label>
-            <input type='text' className='dashboard-text-field '/>
+          
+          <div className='md:game-card-md lg:game-card-lg h-full'>
+            {isDisplayingPlayedGames ? <GamePlayed GamesPlayed={gamesPlayed} /> : <GameWishlist GamesWishlist={gamesWishlist} />}
           </div>
-        </div>
-
-        <div className='my-2'></div>
         
-        <div className='md:game-card-md lg:game-card-lg h-full'>
-          {isDisplayingPlayedGames ? <GamePlayed GamesPlayed={gamesPlayed} /> : <GameWishlist GamesWishlist={gamesWishlist} />}
-        </div>
-       
-        <div className='my-16'></div>
-        
+          <div className='my-16'></div>
+          
 
-        <div className='dashboard-add-game-banner'>
-            <h1 className='dashboard-add-game-text'>Add Game</h1>
-            <div className='dashboard-format-add-game'>
-              <button className='dashboard-add-game-btn' onClick={(e) => {
-                  e.preventDefault()
-                  setDisplayAddGame(true)
-                }}>+</button>
-            </div>
+          <div className='dashboard-add-game-banner'>
+              <h1 className='dashboard-add-game-text'>Add Game</h1>
+              <div className='dashboard-format-add-game'>
+                <button className='dashboard-add-game-btn' onClick={(e) => {
+                    e.preventDefault()
+                    setDisplayAddGame(true)
+                  }}>+</button>
+              </div>
+          </div>
+          {displayAddGame ? <AddGame /> : null}
+          {displayEditGame.display ? <EditGame gameID={displayEditGame.gameID} gameTitle={displayEditGame.gameTitle}/> : null}
         </div>
-        {displayAddGame ? <AddGame /> : null}
-      </div>
-    </DisplayAddGame.Provider>
-    
+      </DisplayAddGame.Provider>
+    </DisplayEditGame.Provider>
   )
 }
 
