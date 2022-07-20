55613dc09b40426e3f6ea7c7fa573e559e05eb31
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-07-20 15:08:45 -0700

    added state and context for displayeditgame

--- client/src/components/GamePlayed.js
+++ client/src/components/GamePlayed.js
@@ -1,37 +1,42 @@
 import React, { useState, useContext } from 'react'
+import { DisplayEditGame } from '../context/DisplayEditGame'
 
 const GamePlayed = ({ GamesPlayed }) => {
     let gid = []
-    const [test, setTest] = useState(false)
+    const { displayEditGame, setDisplayEditGame } = useContext(DisplayEditGame)
+    
 
     return GamesPlayed.map((game, index) => {
         gid.push(game._id)
         
         return(
             <div key={index} className='p-4 py-10 md:p-10'>
-            <div className='dashboard-game-section'>
-                <div className='dashboard-game-img-box'>
-                <img src={game.game_screenshot_uri} alt='game-screenshot' className='dashboard-game-img'/>
+                <div className='dashboard-game-section'>
+                    <div className='dashboard-game-img-box'>
+                    <img src={game.game_screenshot_uri} alt='game-screenshot' className='dashboard-game-img'/>
+                    </div>
+                    
+                    <div className='dashboard-game-textbox relative'>
+                    <div className='dashboard-game-textbox-title-rating-section'>
+                        <h1 className='dashboard-game-title'>{game.game_title}</h1>
+                        <h1 className='dashboard-game-rating'>{game.game_rating} / 10 ★</h1>
+                    </div>
+                    <div className='dashboard-game-textbox-subinfo-section'>
+                    <h1 className='dashboard-game-subtext-heading'>Released Date: <p className='dashboard-game-subtext-info'>{game.game_release_date}</p></h1>
+                        <h1 className='dashboard-game-subtext-heading'>Status: <p className='dashboard-game-subtext-info'>{game.game_status}</p></h1>
+                        <h1 className='dashboard-game-subtext-heading'>Hours Played: <p className='dashboard-game-subtext-info'>{game.game_hours_played}</p></h1>
+                    </div>
+                    
+                    <button className='dashboard-game-edit-btn' onClick={() => {
+                            console.log(gid[index])
+                            setDisplayEditGame({
+                                display: true,
+                                gameID: gid[index],
+                                gameTitle: game.game_title
+                            })
+                        }}>Edit</button>
+                    </div>
                 </div>
-                
-                <div className='dashboard-game-textbox relative'>
-                <div className='dashboard-game-textbox-title-rating-section'>
-                    <h1 className='dashboard-game-title'>{game.game_title}</h1>
-                    <h1 className='dashboard-game-rating'>{game.game_rating} / 10 ★</h1>
-                </div>
-                <div className='dashboard-game-textbox-subinfo-section'>
-                <h1 className='dashboard-game-subtext-heading'>Released Date: <p className='dashboard-game-subtext-info'>{game.game_release_date}</p></h1>
-                    <h1 className='dashboard-game-subtext-heading'>Status: <p className='dashboard-game-subtext-info'>{game.game_status}</p></h1>
-                    <h1 className='dashboard-game-subtext-heading'>Hours Played: <p className='dashboard-game-subtext-info'>{game.game_hours_played}</p></h1>
-                </div>
-                
-                <button className='dashboard-game-edit-btn' onClick={() => {
-                        console.log(gid[index])
-                        setTest(prev => !prev)
-                    }}>Edit</button>
-                </div>
-                {<>{test ? <>dasf</> : <>fgsd</> }</>}
-            </div>
             </div>
         )
         }
