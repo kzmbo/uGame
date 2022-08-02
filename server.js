a92dd1ff4a44d57f28b17b33a4245f0762af3df9
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-08-02 00:44:57 -0700

    config server for deployment

--- server.js
+++ server.js
@@ -261,9 +261,9 @@ app.delete('/deletewishlistgame', verifyUser, async (req, res) => {
 
 let port = process.env.PORT || 4000
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('../client/build'))
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+}
 
 app.listen(port, () => {
     console.log("API server running on port " + port)
