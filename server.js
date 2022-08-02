2838ca2ec574b39bc9e2c2583f8f83a1bc30521f
kzmbo <65250474+kzmbo@users.noreply.github.com>
2022-08-02 01:16:46 -0700

    edit file path

--- server.js
+++ server.js
@@ -261,7 +261,7 @@ app.delete('/deletewishlistgame', verifyUser, async (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('/client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
